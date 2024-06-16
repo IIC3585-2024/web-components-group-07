7588b3a16668dc1d141e186868d4029bb9807d2e
Pedro Palma <pedro.palma@uc.cl>
2024-06-15 21:12:53 -0400

    chore: Use Lit via npm instead of CDN

--- src/components/lit-todo-list.js
+++ src/components/lit-todo-list.js
@@ -1,4 +1,4 @@
-import { LitElement, html, css } from 'https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js';
+import { LitElement, html, css } from 'lit';
 
 class LitTodoList extends LitElement {
   static styles = css`
