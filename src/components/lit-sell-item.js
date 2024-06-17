d3140e156af2b90c5cee5d9a62819e3708460e9a
donjavi-538 <javier.v.armijo@uc.cl>
2024-06-17 01:33:12 -0400

    feat: updated style

--- src/components/lit-sell-item.js
+++ src/components/lit-sell-item.js
@@ -3,74 +3,84 @@ import { LitElement, html, css } from 'lit';
 class LitSellItem extends LitElement {
   static styles = css`
     .wrapper {
-      max-width: 300px;
-      margin: auto;
-      padding: 10px;
-      box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
-      background-color: white;
-      border-radius: 5px;
-      text-align: center;
-      box-sizing: border-box;
-      float: left;
+        position: relative;
+        font-family: Arial, Helvetica, sans-serif;
+        max-width: 300px;
+        margin: 5px;
+        padding: 10px;
+        box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
+        background-color: white;
+        border-radius: 5px;
+        text-align: center;
+        box-sizing: border-box;
+        float: left;
     }
     .img {
-      width: 100%;
-      height: 150px;
-      text-align: center;
-      object-fit: cover;
-      border-radius: 5px;
-      display: block;
-    }
-    .attr-div {
-      margin-top: 10px;
+        width: 100%;
+        height: 150px;
+        text-align: center;
+        object-fit: cover;
+        border-radius: 5px;
+        display: block;
     }
     .name {
-      font-size: 1.2em;
-      text-align: center;
-      margin: 10px 0;
+        font-size: 1.2em;
+        text-align: center;
+        margin: 10px 0;
     }
     .price-div {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      min-height: 70px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        min-height: 50px;
     }
     .price {
-      font-size: 1.5em;
-      margin: 0;
+        font-size: 1.5em;
+        margin: 0;
     }
     .discount-div {
-      display: flex;
-      flex-direction: row;
-      margin-left: 40px;
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        background-color: red;
+        color: white;
+        padding: 5px 10px;
+        font-size: 14px;
+        font-weight: bold;
+        border-radius: 5px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+        z-index: 1;
     }
     .discount-price {
-      font-size: 1.5em;
-      margin: 0;
-      color: red;
+        font-size: 1.5em;
+        margin: 0;
+        color: red;
     }
     .discount {
-      font-size: 0.8em;
-      margin: 0;
+        font-size: 0.8em;
+        margin: 0;
+    }
+    .attr-div {
+        margin-top: 10px;
     }
     .star-div {
-      display: flex;
-      justify-content: center;
-      align-items: center;
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
     .star-list {
-      display: flex;
-      list-style: none;
-      padding: 0;
+        display: flex;
+        list-style: none;
+        padding: 0;
     }
     .star-list li {
-      font-size: 1.2em;
-      color: gold;
+        font-size: 1.2em;
+        color: gold;
     }
     .rating {
-      font-size: 1em;
-      margin-left: 5px;
+        font-size: 1em;
+        margin-left: 5px;
     }
     .old-price {
       text-decoration: line-through;
@@ -96,8 +106,8 @@ class LitSellItem extends LitElement {
     return this.discountPrice ? 
       html`
         <p class="price old-price">$${this.price}</p>
+        <p class="discount-price">$${this.discountPrice}</p>
         <div class="discount-div">
-          <p class="discount-price">$${this.discountPrice}</p>
           <p class="discount">
             ${this.discount}% off
           </p>
